b68b79f1995c1ace2b1095996c4e6afd300d3505
denseflux <git@bugs.me.uk>
2023-10-03 16:12:22 +0100

    keep only the composer-addon-installer

--- cli-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'bootstrap.php';
-
-$application = \Laminas\Mvc\Application::init(require 'application/config/application.config.php');
-
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    $application->getServiceManager()->get('Omeka\EntityManager')
-);
